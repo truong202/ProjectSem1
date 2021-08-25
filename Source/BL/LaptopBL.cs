4f813332a4915def007502eb6edf283c38e9f4d3
truong202 <38mavantruong12a4@gmail.com>
2021-08-25 16:38:46 +0700

    update

--- Source/BL/LaptopBL.cs
+++ Source/BL/LaptopBL.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace BL  
 {
     public class LaptopBL
-    {
+    {  
         private LaptopDAL laptopDAL = new LaptopDAL();
         public Laptop GetById(int laptopId)
         {
